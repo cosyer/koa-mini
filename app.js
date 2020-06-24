f968f7bf0b231f67fc4e0bc3dde401fe5e4f13ce
cosyer <chenyu@mydearest.cn>
2020-06-24 15:58:54 +0800

    add: compare with koa

---
+++ app.js
@@ -0,0 +1,23 @@
+const Koa = require("Koa");
+
+const app = new Koa();
+
+const port = process.env.PORT || 7000;
+
+app.use(async (ctx, next) => {
+  console.log("Middleware 1 Start");
+  await next();
+  console.log("Middleware 1 End");
+});
+
+app.use(async (ctx, next) => {
+  console.log("Middleware 2 Start");
+  await next();
+  console.log("Middleware 2 End");
+
+  ctx.body = "hello, world";
+});
+
+app.listen(7000, function () {
+  console.log("Listening on port " + port);
+});
